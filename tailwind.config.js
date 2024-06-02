eb3884be6445fcecf0394091a9edb21850651a5b
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-06-02 12:59:18 +0600

    project almost done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+export default {
+  content: ['./*.{html,js}', './src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily:{
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
